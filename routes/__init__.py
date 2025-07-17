b9212a1d5b2cfc5f70bef798ca32482c3dcaa481
Suraj Kushwah <suraj@bugswriter.com>
2025-07-17 18:56:18 +0530

    adding route

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,10 @@
 from .tree import tree_bp
 from .decrypt import decrypt_bp
 from .search import search_bp
+from .index import index_bp
 
 def register_routes(app):
+    app.register_blueprint(index_bp) 
     app.register_blueprint(tree_bp)
     app.register_blueprint(decrypt_bp)
     app.register_blueprint(search_bp)
